a82546c8e857c08c10c2cd4bfa2a114571e14399
Heder Alves <83372052+HederAlves@users.noreply.github.com>
2021-10-04 23:59:42 -0300

    Update funcao.js
    atualizando calculo da função

--- funcao.js
+++ funcao.js
@@ -2,6 +2,6 @@ function clicar() {
     var num1 = document.querySelector(".peso").value;
     var num2 = document.querySelector(".altura").value;
    
-    var resultado = (parseInt(num1) * parseInt(num2))/2;
+    var resultado = (parseInt(num1) / parseInt(num2))*4;
     document.querySelector(".resultadoImc").innerHTML = resultado;
 }
