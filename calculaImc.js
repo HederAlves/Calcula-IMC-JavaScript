ab1d09f186a29324ce09061d29e11a0706fbff56
Heder Alves <83372052+HederAlves@users.noreply.github.com>
2021-10-06 15:58:49 -0300

    Delete calculaImc.js
    Excluindo  todos os arquivos corrigindo bug, no responsivo.

--- calculaImc.js
+++
@@ -1,7 +0,0 @@
-function calculaImc() {
-    var altura = document.querySelector(".altura").value;
-    var peso = document.querySelector(".peso").value;
-   
-    var resultadoImc = parseFloat(peso) / (parseFloat(altura) * parseFloat(altura));
-    document.querySelector(".resultadoImc").innerHTML = resultadoImc.toFixed(2);
-}
