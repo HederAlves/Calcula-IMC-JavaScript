e89a1c7ee2f27edc02b0b65613bcc67d57e33eb8
Heder Alves <83372052+HederAlves@users.noreply.github.com>
2021-10-08 23:37:44 -0300

    Update calculaImc.js
    Adicionando nova funcionalidade ao programa, filtro para indicar em qual nível usuário se enquadra.
    Melhorando código e semântica.

--- calculaImc.js
+++ calculaImc.js
@@ -1,7 +1,38 @@
 function calculaImc() {
-    var altura = document.querySelector(".altura").value;
-    var peso = document.querySelector(".peso").value;
+
+    const altura = document.querySelector(".altura").value;
+    const peso = document.querySelector(".peso").value;
    
-    var resultadoImc = parseFloat(peso) / (parseFloat(altura) * parseFloat(altura));
-    document.querySelector(".resultadoImc").innerHTML = resultadoImc.toFixed(2);
+    const resultadoImc = parseFloat(peso) / (parseFloat(altura) * parseFloat(altura));
+    document.querySelector('[data-resultado-imc]').innerHTML = resultadoImc.toFixed(2);
+
+
+    if (resultadoImc < 18.5) {
+
+        document.querySelector('[data-resultado-filtro]').innerHTML = console.log("Magreza");
+    }
+
+    if (resultadoImc >= 18.5 || resultadoImc <= 24.9) {
+
+       document.querySelector('[data-resultado-filtro]').innerHTML = console.log("Normal");
+     }
+
+     if (resultadoImc >= 25 || resultadoImc <= 29.9) {
+
+       document.querySelector('[data-resultado-filtro]').innerHTML-console.log("Sobrepeso");
+     }
+
+     if (resultadoImc >= 30 || resultadoImc <= 39.9) {
+
+       document.querySelector('[data-resultado-filtro]').innerHTML = console.log("Obesidade");
+     }
+
+     if (resultadoImc > 40) {
+
+       document.querySelector('[data-resultado-filtro]').innerHTML = console.log("Obesidadegrave");
+     }
+
+
+
 }
+  
